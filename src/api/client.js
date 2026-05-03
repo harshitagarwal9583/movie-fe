b9b849a37a383a1d2b655e05c12f2d1cbd720f1b
HARSHITAGARWAL9583 <harshit.aga29@gmail.com>
2026-05-03 19:56:26 +0530

    add backend  url

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://movie-be-rsbm.onrender.com/api';
+
 
 const client = axios.create({
   baseURL: API_BASE_URL,
